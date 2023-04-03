39bd301930856a86369a0a0629484c9f4eedebe7
Pavel Siarzhantau <pavel.siarzhantau@gmail.com>
2023-04-03 23:28:25 +0200

    adjusted before merge

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
-const start = new Date();
+onst start = new Date();
 const pastDate = new Date(Date.UTC(2000, 0, 0, 0, 0, 0));
 console.log('pastDate::  ' + start);
 console.log('start::  ' + pastDate);
 console.log('data parse:: ' + Date.parse(pastDate));
 console.log('date now::  ' + Date.now());
 console.log(Date.now() - Date.parse(pastDate));
-console.log(new Date(Date.now() - Date.parse(pastDate)).toISOString().slice(11,19));
+console.log(new Date(Date.now() - Date.parse(start)).toISOString().slice(11,19));
 console.log('');
 let animal = new Object();
 animal.name = 'Rex';
 animal.burk = () => { console.log('Woof!' + this) };
-
+//test
 let animal1 = {
     name: 'Tuz',
     burk1:  function() {
